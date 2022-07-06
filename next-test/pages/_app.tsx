951821c5e29bf87f7aac853625b772da152d13af
alastair-buchanan <55517474+alastair-buchanan@users.noreply.github.com>
2022-07-06 17:24:17 +1000

    Added router and static props functionality
    Added router functionality and passing props between pages. Added get static props

--- next-test/pages/_app.tsx
+++ next-test/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {Hydrate, QueryClient, QueryClientProvider} from "react-query"
+import React from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+function MyApp({Component, pageProps}: AppProps) {
+    const [queryClient] = React.useState(() => new QueryClient());
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+                <Component {...pageProps} />
+            </Hydrate>
+        </QueryClientProvider>
+    )
 }
 
 export default MyApp
