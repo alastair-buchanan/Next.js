debf8850691110866d59ef0f288798ddbbdc0844
alastair-buchanan <55517474+alastair-buchanan@users.noreply.github.com>
2022-07-07 20:12:46 +1000

    Added User Table and profile screens
    Added User Table and profile screens

---
+++ next-test/components/UserTable.tsx
@@ -0,0 +1,100 @@
+import {FC} from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import TableContainer from "@mui/material/TableContainer";
+import Link from "next/link";
+import {dehydrate, QueryClient, useQuery} from "react-query";
+import Container from "@mui/material/Container";
+import Head from "next/head";
+import {makeStyles, Toolbar, Typography} from "@mui/material";
+import {useRouter} from "next/router";
+
+interface IProps {}
+
+export type User = {
+    id: number,
+    firstName: string,
+    lastName: string,
+    address: string,
+    phone: string,
+    dateOfBirth: string,
+    picture: string,
+}
+
+export type Data = {
+    users: User[]
+}
+
+const fetchAllUsers = async () => await (await fetch("http://localhost:3000/api/users")).json();
+
+export const UserTable: FC<IProps> = () => {
+    const {data, error, status} = useQuery<Data>('users', fetchAllUsers);
+    const router = useRouter();
+
+    function handleClick(user: User) {
+        router.push(
+            {
+                pathname: '/profile',
+                query: {
+                    id: user.id
+                }
+            },
+            '/profile',
+        )
+    }
+
+    if (status == "loading") return <div>loading</div>
+
+    if (!data) return <div>No Data!</div>
+
+    return (
+        <Container>
+            <Toolbar><Typography
+                sx={{ flex: '1 1 100%' }}
+                variant="h6"
+                id="tableTitle"
+                component="div"
+            >
+                Users
+            </Typography></Toolbar>
+            <TableContainer sx={{flexGrow: 1, maxHeight: window.innerHeight*.6}} component={Paper}>
+                <Table sx={{minWidth: 650}} size="small" aria-label="simple table">
+                    <TableHead>
+                        <TableRow  >
+                            <TableCell sx={{fontWeight: 600}}>Name</TableCell>
+                            <TableCell sx={{fontWeight: 600}} align="right">Address</TableCell>
+                            <TableCell sx={{fontWeight: 600}} align="right">Phone number</TableCell>
+                            <TableCell sx={{fontWeight: 600}} align="right">Date of birth</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data?.users.map((user) => (
+                                <TableRow key={user.id} onClick={() => handleClick(user)} sx={{":hover": {backgroundColor: "#EEE"}, '&:last-child td, &:last-child th': {border: 0}}}>
+                                    <TableCell sx={{flexGrow: 1}} component="th" scope="row">{user.firstName + " " + user.lastName}</TableCell>
+                                    <TableCell sx={{flexGrow: 1}} align="right">{user.address}</TableCell>
+                                    <TableCell sx={{flexGrow: 1}} align="right">{user.phone}</TableCell>
+                                    <TableCell sx={{flexGrow: 1}} align="right">{user.dateOfBirth}</TableCell>
+                                </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Container>
+    )
+}
+
+export async function getStaticProps() {
+    const queryClient = new QueryClient();
+
+    await queryClient.prefetchQuery<Data>('users', fetchAllUsers)
+
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient)
+        }
+    }
+}
---
+++ next-test/pages/profile.tsx
@@ -0,0 +1,63 @@
+import {NextPageWithLayout} from "./_app";
+import {useRouter} from "next/router";
+import {ReactElement, useState} from "react";
+import Layout from "../components/Layout";
+import {Card, CardActionArea, CardContent, CardMedia, Grid, Typography} from "@mui/material";
+import Image from "next/image";
+import {useQuery} from "react-query";
+import {Data, User} from "../components/UserTable";
+
+const Profile: NextPageWithLayout = () => {
+    const router = useRouter();
+    const query = router.query;
+    const fetchUser = async () => await (await fetch(`http://localhost:3000/api/users/${query.id}`)).json();
+    const {data, error, status} = useQuery<Data>('user', fetchUser);
+    const user: User | undefined = data?.users[0];
+
+    if (status == "loading") return <div>loading</div>
+
+    if (error) return <div>Error returning User information</div>
+
+    return (
+        <Card sx={{flexGrow: 1, maxHeight: 450, maxWidth: 700}}>
+            {
+                !user
+                    ? <div>Error returning User information</div>
+                    : <CardActionArea>
+                        <Grid container spacing={2}>
+                            <Grid item>
+                                <Image layout='intrinsic' src={`${user.picture}`} width={150} height={150}/>
+                            </Grid>
+                            <Grid item>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        Name: {user.firstName + " " + user.lastName}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Address: {user.address}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Phone: {user.phone}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Date of Birth: {user.dateOfBirth}
+                                    </Typography>
+
+                                </CardContent>
+                            </Grid>
+                        </Grid>
+                    </CardActionArea>
+            }
+        </Card>
+    )
+}
+
+Profile.getLayout = function getLayout(page: ReactElement) {
+    return (
+        <Layout>
+            {page}
+        </Layout>
+    )
+}
+
+export default Profile
